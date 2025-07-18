313bb6690fc31c0da40a3381e0206ce20550b070
kimmyxpow <abinovalfauzi@proton.me>
2025-07-18 15:13:56 +0700

    ⚙️ chore: add publicDir argument to PocketBase command

--- pb/main.go
+++ pb/main.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/pocketbase/pocketbase"
 )
 
+func defaultPublicDir() string {
+	if strings.HasPrefix(os.Args[0], os.TempDir()) {
+		// most likely ran with go run
+		return "./pb_public"
+	}
+
+	return filepath.Join(os.Args[0], "../pb_public")
+}
+
 func main() {
 	// Initialise a new PocketBase app instance.
 	app := pocketbase.New()
+
+	var publicDir string
+	app.RootCmd.PersistentFlags().StringVar(
+		&publicDir,
+		"publicDir",
+		defaultPublicDir(),
+		"the directory to serve static files",
+	)
 
 	// Start the PocketBase app server, and handle any errors.
 	if err := app.Start(); err != nil {
